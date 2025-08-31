652d879506ca970f159b1465866ec5df95c69e8d
Aldair Sinay <lestersinay100@gmail.com>
2025-08-31 13:16:19 -0600

    Primer commit: Script base de Shodan Guatemala

---
+++ shodan_guatemala.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import shodan
+from collections import defaultdict
+
+# === DATOS DEL ESTUDIANTE ===
+CARNET = "202012345"
+NOMBRE = "Aldair Sinay"
+CURSO = "Seguridad Informática"
+SECCION = "B"
